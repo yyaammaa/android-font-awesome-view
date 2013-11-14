5c89fef8772c982013761ed8e1d352d4addd3865
yamabit <yama@thebitcellar.com>
2013-11-14 13:21:07 +0900

    initial commit

---
+++ FontAwesomeView/src/com/stackrmobile/awesomefont/IconView.java
@@ -0,0 +1,25 @@
+
+package com.stackrmobile.awesomefont;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class IconView extends View {
+
+  public IconView(Context context, AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+    // TODO Auto-generated constructor stub
+  }
+
+  public IconView(Context context, AttributeSet attrs) {
+    super(context, attrs);
+    // TODO Auto-generated constructor stub
+  }
+
+  public IconView(Context context) {
+    super(context);
+    // TODO Auto-generated constructor stub
+  }
+
+}
