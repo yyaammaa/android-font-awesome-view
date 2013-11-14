b213953172619a1f3f9ac476252a89bdab40c77c
yamabit <yama@thebitcellar.com>
2013-11-14 15:33:30 +0900

    sample

--- FontAwesomeView/src/com/stackrmobile/fontawesomeview/IconView.java
+++ FontAwesomeView/src/com/stackrmobile/fontawesomeview/IconView.java
@@ -2,24 +2,54 @@
 package com.stackrmobile.fontawesomeview;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.util.AttributeSet;
-import android.view.View;
+import android.util.Log;
+import android.widget.TextView;
 
-public class IconView extends View {
+import java.util.Map;
 
-  public IconView(Context context, AttributeSet attrs, int defStyleAttr) {
-    super(context, attrs, defStyleAttr);
-    // TODO Auto-generated constructor stub
-  }
+public class IconView extends TextView {
 
-  public IconView(Context context, AttributeSet attrs) {
-    super(context, attrs);
-    // TODO Auto-generated constructor stub
+  private static final String TAG = "IconView";
+
+  private static Typeface sTypeface;
+  private static Map<String, String> sCharMap;
+
+  static {
+    sCharMap = FontAwesome.getCharMap();
   }
 
   public IconView(Context context) {
     super(context);
-    // TODO Auto-generated constructor stub
+    init();
+  }
+
+  public IconView(Context context, AttributeSet attrs) {
+    super(context, attrs);
+    init();
+  }
+
+  public IconView(Context context, AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+    init();
+  }
+
+  private void init() {
+    loadFont(getContext());
+    setTypeface(sTypeface);
+  }
+
+  private static void loadFont(Context context) {
+    if (sTypeface == null) {
+      try {
+        // TODO: project libに置けるかどうか試す
+        sTypeface = Typeface.createFromAsset(context.getAssets(), "fontawesome-webfont.ttf");
+      } catch (Exception e) {
+        Log.e(TAG, "Failed to load font: " + e.getMessage());
+        sTypeface = Typeface.DEFAULT;
+      }
+    }
   }
 
 }
--- FontAwesomeViewSample/src/com/stackrmobile/fontawesomeview/sample/MainActivity.java
+++ FontAwesomeViewSample/src/com/stackrmobile/fontawesomeview/sample/MainActivity.java
@@ -1,9 +1,22 @@
 
 package com.stackrmobile.fontawesomeview.sample;
 
+import com.stackrmobile.fontawesomeview.FontAwesome;
+import com.stackrmobile.fontawesomeview.IconView;
+
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 public class MainActivity extends Activity {
 
@@ -11,11 +24,92 @@ public class MainActivity extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    Map<String, String> map = FontAwesome.getCharMap();
+    ArrayList<Map.Entry<String, String>> list = new ArrayList<Map.Entry<String, String>>();
+    list.addAll(map.entrySet());
+
+    GridView gridView = (GridView) findViewById(R.id.activity_main_grid_view);
+    IconAdapter adapter = new IconAdapter(this);
+    gridView.setAdapter(adapter);
+    adapter.setList(list);
   }
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
     return false;
+  }
+
+  private class IconAdapter extends BaseAdapter {
+
+    private Context mContext;
+    private LayoutInflater mInflater;
+    private ArrayList<Map.Entry<String, String>> mList;
+
+    private IconAdapter(Activity activity) {
+      mContext = activity;
+      mInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+      mList = new ArrayList<Map.Entry<String, String>>();
+    }
+
+    private void setList(ArrayList<Map.Entry<String, String>> list) {
+      mList = list;
+    }
+
+    @Override
+    public int getCount() {
+      if (mList == null) {
+        return 0;
+      }
+      return mList.size();
+    }
+
+    @Override
+    public Map.Entry<String, String> getItem(int position) {
+      if (mList == null) {
+        return null;
+      }
+      return mList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+      return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+      Map.Entry<String, String> entry = getItem(position);
+      ViewHolder holder;
+      if (convertView != null) {
+        holder = (ViewHolder) convertView.getTag();
+      } else {
+        convertView = mInflater.inflate(R.layout.grid_view_item, parent, false);
+        holder = new ViewHolder(convertView);
+        convertView.setTag(holder);
+      }
+
+      holder.mIconView.setText(entry.getValue());
+      holder.mNameView.setText(entry.getKey());
+      // TODO: 表示する
+      holder.mUnicodeView.setText(entry.getValue());
+
+      return convertView;
+    }
+
+  }
+
+  private static class ViewHolder {
+
+    IconView mIconView;
+    TextView mNameView;
+    TextView mUnicodeView;
+
+    private ViewHolder(View view) {
+      mIconView = (IconView) view.findViewById(R.id.grid_view_icon_text);
+      mNameView = (TextView) view.findViewById(R.id.grid_view_icon_name);
+      mUnicodeView = (TextView) view.findViewById(R.id.grid_view_icon_unicode);
+    }
   }
 
 }
